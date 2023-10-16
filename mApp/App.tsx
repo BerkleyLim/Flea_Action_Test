ac9fd03e38107447cdc1383af0dbe03636c78a03
berkleylim <berkleylim16@gmail.com>
2023-10-17 00:11:20 +0900

    test: RN 연습 - 화면 추가 해보기 - 내일 화면 구성 후 API 연동 예정 - 이후, github Readme.md 꾸미기

--- mApp/App.tsx
+++ mApp/App.tsx
@@ -5,114 +5,66 @@
  * @format
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import type {PropsWithChildren} from 'react';
+import {QueryClient, QueryClientProvider, useQuery} from 'react-query';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import { ThemeProvider } from '@react-navigation/native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const headerText = {
+  // fontStyle: "italic",
+  // fontWeidht: "bold"
+  fontSize:10,
 }
+
+const headerStyle ={
+  backgroundColor: "yellow",
+}
+
+const Header = () => {
+  return(
+    <View style={headerStyle}>
+      <Text style={headerText}>텍스트</Text>
+      <Text style={headerText}>텍스트</Text>
+    </View>
+  )
+}
+
+const Body = () => {
+  // const result = useQuery('posts', postData);
+  // const { data, error, isLoading } = result;
+
+  // if (isLoading) return <Component>...</Component>;
+  // if (error) return <Component>...</Component>;
+
+  return (
+    <View>
+      <Button title={"hi"} 
+          // onPress=}
+      ></Button>
+    </View>
+  )
+}
+
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+    // Provider Setting
+    <QueryClientProvider client={queryClient}> 
+      <SafeAreaView>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        <Body></Body>
+      </SafeAreaView>
+    </QueryClientProvider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
