152e3089e923df8f422f15a4ec8f054014ce0b36
berkleylim <berkleylim16@gmail.com>
2023-10-21 15:37:19 +0900

    test: navigationContainer 설정 재설정 - 아직은 NatigationContainer는 돌아가지 않음 - 차후, 쓰게 된다면 코드 정리하고, 안될것 같으면 Web 버전 react-router-dom 방식처럼 개발 진행 예정

--- mApp/App.tsx
+++ mApp/App.tsx
@@ -8,31 +8,35 @@ import "react-native-gesture-handler";
 import React from 'react';
 import {QueryClient, QueryClientProvider, useQuery} from 'react-query';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Body from './src/page/component/Body'
 import Header from './src/page/component/common/Header'
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, StyleSheet } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, View } from "react-native";
 
 const queryClient = new QueryClient();
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
   // 코드 설명 : 아래는 NavigationContainer 설정하여 각각 스크린마다 이동 시 설정한다.
   // <QueryClientProvider client={queryClient}>
-  //   <SafeAreaView>
-  //     <Header></Header>
-  //     <NavigationContainer>
-  //       <Stack.Navigator initialRouteName="Body">
-  //       {/* <Stack.Navigator> */}
-  //         <Stack.Screen name="Body" component={Body} />
-  //       </Stack.Navigator>
-  //     </NavigationContainer>
-  //   </SafeAreaView>
+    // // <SafeAreaView style={styles.appStyle}>
+    // <View style={{flex:1}}>
+    //   <ScrollView style={styles.scrollView}>
+    //     <Header></Header>
+    //     <NavigationContainer>
+    //       <Stack.Navigator initialRouteName="Body">
+    //       {/* <Stack.Navigator>  */}
+    //         <Stack.Screen name="Body" component={Body} />
+    //       </Stack.Navigator>
+    //     </NavigationContainer>
+    //   </ScrollView>
+    // </View>
   // </QueryClientProvider>
+ 
     // Provider Setting
     <QueryClientProvider client={queryClient}> 
       <SafeAreaView>
@@ -46,17 +50,26 @@ function App(): JSX.Element {
   );
 }
 
+// CSS 꾸미기
+let screenWidth = Dimensions.get('window').width;
+let screenHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
+    // flex: 1,
+    // width: screenWidth,
+    // height: screenHeight,
+    // height: screenHeight,
+    zIndex: 0,
     flex: 1,
   },
   scrollView: {
     backgroundColor: 'orange',
     // marginHorizontal: 20,
+    // height: screenHeight
   },
   text: {
     fontSize: 42
-  }
+  },
 });
 
 export default App;
