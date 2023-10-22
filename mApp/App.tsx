47607d926c45048829a672fad6dd64d3c3fcbf3e
berkleylim <berkleylim16@gmail.com>
2023-10-22 22:56:26 +0900

    comment: 각 함수의 로직의 대한 설명 작성 - App.tsx의 대해 알기 쉽게 작성함

--- mApp/App.tsx
+++ mApp/App.tsx
@@ -30,23 +30,32 @@ type sseParameter = {
 
 // 이 함수는 SSE 연동할 때 쓰는 환경 설정용 함수이다
 const sse = ({sortData, setSortData, sortReverseData, setSortReverseData}:sseParameter) => {
+  // 여기서는 SSE Event Type과 맞춰야 하므로 custom을 써야한다.
   type FleaCustomEvents = "sse.contents_viewed" | "sse.auction_viewed";
   const source = new EventSource<FleaCustomEvents>("https://api.fleaauction.world/v2/sse/event");
   
 
   const listener: EventSourceListener<FleaCustomEvents> = (e:any) => {
     if (e.type === 'open') {
+      // SSE를 연다
       console.log('open')
     } else if (e.type === 'message') {
+      // event type이 message일 경우 수행
       console.log('message')
-    } else if (e.type === 'sse.contents.viewed') {
+    } else if (e.type === 'sse.contents_viewed') {
+      // event type이 sse.contents_viewed 일경우 수행
+      // 현재 LogCat으로 확인할 경우 로그는 돌아가지만, 인증을 넣어야 수행 가능하다.
       console.log('sse.contents.viewed, 현재 403 에러로 연결 불가능')
     } else if (e.type === 'sse.auction_viewed') {
+      // event type이 sse.auction_viewed일 경우 수행
+      // 이부분이 핵심이 되는 부분이다.
       console.log('sse.auction_viewed')
 
       const d = JSON.parse(e.data) as Data;
 
       console.log(d)
+
+      // state 값을 저장하는 로직, 여기서는 오름차순, 내림차순 데이터를 저장한다.
       setSortData((prevData:any) => [...prevData, d]);
       setSortReverseData((prevData:any) => [...prevData, d]);
 
@@ -80,6 +89,7 @@ function App(): JSX.Element {
       setRefreshing(false);
     }, 2000);
 
+    // 오름 차순 로직 실시, 새로고침 시 state 값 날라가서 AsyncStorage 사용
     let tempData;
     await AsyncStorage.getItem('sortData', (err, result) => {
       console.log(result)
@@ -90,6 +100,7 @@ function App(): JSX.Element {
     tempData = tempData.sort((a,b) => a.auctionId - b.auctionId);
     setSortData(tempData);
     
+    // 내림 차순 로직 실시, 새로고침 시 state 값 날라가서 AsyncStorage 사용
     await AsyncStorage.getItem('sortReverseData', (err, result) => {
       console.log(result)
       tempData = JSON.parse(result);
